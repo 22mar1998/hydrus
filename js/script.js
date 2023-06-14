bdfe8e0297a5c861d7d502a4c2a4c433c97b618e
22mar1998 <114736946+22mar1998@users.noreply.github.com>
2023-06-14 16:21:11 +0530

    ddddd

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// gsap (animations)
+
+const timeline = gsap.timeline({ defaults: { duration: 1 } })
+timeline
+    .from('#logo', { y: -100 })
+    .from('.hero-text', { opacity: 0 })
+    .from('.hero-img', { opacity: 0 })
+    .from('.nav-links a', { opacity: 0, stagger: .3 })
+
+
+
+
+// jquery (menu)
+
+$(document).ready(function () {
+    $(".menu-btn").click(function () {
+        $(".nav-links").show(300);
+    });
+});
+
+$(document).ready(function () {
+    $(".menu-btn-close").click(function () {
+        $(".nav-links").hide(300);
+    });
+});
+
